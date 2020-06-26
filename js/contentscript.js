ca4f3c0cbdbcf4cdfea4ac920a5383775320fc00
archer-jay <archer_jay@icloud.com>
2020-06-26 08:02:06 -0700

    regular update
    several changes made, nothing concrete.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,7 +1,12 @@
-const styleLocs = document.createElement('link')
-styleLocs.rel = "stylesheet"
-styleLocs.type = "text/css"
-styleLocs.href = chrome.runtime.getURL('dark-nord-theme-bulk-updater.css');
+const styleReset = document.createElement('link')
+styleReset.rel = "stylesheet"
+styleReset.type = "text/css"
+styleReset.href = chrome.runtime.getURL('normalize.css');
+
+const styleBulk = document.createElement('link')
+styleBulk.rel = "stylesheet"
+styleBulk.type = "text/css"
+styleBulk.href = chrome.runtime.getURL('dark-nord-theme-bulk-updater.css');
 
 const style = document.createElement('link')
 style.rel = "stylesheet"
@@ -9,5 +14,6 @@ style.type = "text/css"
 style.href = chrome.runtime.getURL('NordDark.css');
 
 console.log(style.href);
-document.body.appendChild(styleLocs);
+document.body.appendChild(styleReset);
+document.body.appendChild(styleBulk);
 document.body.appendChild(style);
